b15d37e6ceb4a4a470073ec6fda460d98cf12e99
Leandro Zuza <lendro.zuza@connectbr.dev>
2025-04-30 13:10:52 -0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: [
+      'early-pens-flash.loca.lt',
+      '.loca.lt' // This will allow all localtunnel subdomains
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
